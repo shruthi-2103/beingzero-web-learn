74943264b2277600f5770cc975e263daac7a3eed
Shruthi_2103 <shruthichouhan2001@gmail.com>
2021-04-30 10:33:12 +0530

    JavaScript/CSS/HTML

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const app = express();
+
+app.use(express.static(--dirname+"/frontend"));
 //this is home handlergit
 app.get("/", function(req, res){
     res.send("Welcome to Shruthi's Basic site");
 })
 app.get("/resume",function(req,res){
-    file=__dirname+"/resume.html";
+    file=__dirname+"/frontend/html/resume.html";
     res.sendFile(file);
 })
 // Heroku will automatically set an environment variable called PORT
