4b4106727b70c82fbd9ef65f099eb870d8c9f2db
Shruthi_2103 <shruthichouhan2001@gmail.com>
2021-04-30 10:40:59 +0530

    resume

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(--dirname+"/frontend"));
 //this is home handlergit
 app.get("/", function(req, res){
     res.send("Welcome to Shruthi's Basic site");
